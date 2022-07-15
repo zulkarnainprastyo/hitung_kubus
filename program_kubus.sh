fbff9f31437eabd74d3697e0f7996319c938892e
Zulkarnain Prastyo <zulkarnain.prastyo@binus.ac.id>
2022-07-15 22:00:04 +0700

    Tambah Function Luas

--- program_kubus.sh
+++ program_kubus.sh
@@ -7,9 +7,14 @@ keliling(){
 			echo "Keliling Kubus = " $k
 }
 
+luas(){
+		l=$((6*($1*$1)))
+			echo "Luas Kubus = " $l
+}
+
+
 read -p "Masukkan Panjang Rusuk (r): " s
 
 echo -e "\n\nHasil Perhitungan Kubus dengan Panjang rusuk = $s adalah"
 keliling $s
-
-
+luas $s
